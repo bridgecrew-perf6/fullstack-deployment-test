f868b3e8c0f88971ed5f17127fae231ef573d52d
nlemberg <naama.lemberg@gmail.com>
2022-05-14 18:45:02 +0300

    server commit

---
+++ server/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+   mongoURI: 'mongodb+srv://naama123:naama123@fullstack-deployment-te.nwqnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' 
+}
---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const items = require('./routes/api/items')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+  app.use('/api/items', items)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
