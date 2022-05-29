7a839bd5318c11c1a2cbb80f5556655e511fa0c3
nlemberg <naama.lemberg@gmail.com>
2022-05-29 14:08:53 +0300

    item model path change

--- server/routes/api/items.js
+++ server/routes/api/items.js
@@ -1,9 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-const auth = require("../../middleware/auth")
+const auth = require("../../middleware/auth");
 
-const Item = require("../../models/Item");
+const Item = require("../../models/item");
 
 // GET api/items // get all
 router.get("/", (req, res) => {
