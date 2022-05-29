96eab60c467c680a5eaa962daf0b0fb47bd48a4c
nlemberg <naama.lemberg@gmail.com>
2022-05-29 15:09:54 +0300

    jwt secret config

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,10 +4,14 @@ const jwt = require("jsonwebtoken");
 function auth(req, res, next) {
   token = req.header("x-auth-token");
 
-  if (!token) return res.status(401).json({ msg: "No token, authorization denied" });
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || config.get("jwtSecret")
+    );
 
     req.user = decoded;
     next();
